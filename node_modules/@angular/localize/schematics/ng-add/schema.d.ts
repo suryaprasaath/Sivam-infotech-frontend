66aacfb8f256788c9ec6762053a6954c55c269bb
suryaprasaath <suryaprasaathl.19it@kongu.edu>
2022-08-23 20:10:29 +0530

    first commit

--- node_modules/@angular/localize/schematics/ng-add/schema.d.ts
+++ node_modules/@angular/localize/schematics/ng-add/schema.d.ts
@@ -10,11 +10,16 @@ export interface Schema {
   /**
    * The name of the project.
    */
+  project?: string;
+  /**
+   * The name of the project.
+   * @deprecated use the `project` option instead.
+   */
   name?: string;
   /**
    * Will this project use $localize at runtime?
    *
-   * If true then the dependency is included in the `dependencies` section of packge.json, rather
+   * If true then the dependency is included in the `dependencies` section of package.json, rather
    * than `devDependencies`.
    */
   useAtRuntime?: boolean;
